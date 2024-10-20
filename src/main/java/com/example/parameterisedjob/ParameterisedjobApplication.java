1305bc3bd7c474c65ba05e2be798ef7e51b3056a
priti426 <pritishelke7777@gmail.com>
2024-10-20 16:30:09 +0530

    adding jenkinsfile

---
+++ src/main/java/com/example/parameterisedjob/ParameterisedjobApplication.java
@@ -0,0 +1,13 @@
+package com.example.parameterisedjob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParameterisedjobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParameterisedjobApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/parameterisedjob/ParameterisedjobApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.parameterisedjob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParameterisedjobApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
